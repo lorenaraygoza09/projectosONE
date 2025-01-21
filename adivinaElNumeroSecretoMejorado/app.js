72175bf93f1e22ac720ba9825960f56d0c3eef00
Lorena Raygoza <lorenaraygoza0904@gmail.com>
2025-01-21 16:29:48 -0700

    feat: se termina de añadir la funcionalidad al proyecto

--- adivinaElNumeroSecretoMejorado/app.js
+++ adivinaElNumeroSecretoMejorado/app.js
@@ -1,4 +1,6 @@
 let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
+
 
 //Funcion para ingresar a las etiquetas del html y asignarles un texto
 //declaramos la funcion con 2 elementos
@@ -14,12 +16,29 @@ function verificarIntento(){
     //getElementById devuelve un objeto, nosotros necesitamos el valor, por ello añadimos .value 
     //añadimos parseInt ya que estaba retornando un string y nosotros nececitamos un numero
 let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
-console.log(typeof (numeroUsuario));
-console.log(numeroUsuario);
-console.log(numeroSecreto);
-console.log(numeroUsuario === numeroSecreto);
+console.log(intentos);
+
+if (numeroUsuario === numeroSecreto){
+    asignarTextoElemento('p', `Acertaste el número secreto en ${intentos} ${(intentos ===1 ) ? "intento" : "intentos"}`);
+    //habilita el boton de nuevo juego cuando se acierta el número
+    document.getElementById('reiniciar').removeAttribute('disabled');
+} else {
+    if(numeroUsuario > numeroSecreto){
+        asignarTextoElemento('p', "El número secreto es menor")
+    } else {
+        asignarTextoElemento('p', "El número secreto es mayor");
+    }
+    intentos++;
+    limpiarCaja();
+}
+return;
 }
 
+function limpiarCaja(){
+    let valorCaja = document.querySelector('#valorUsuario');
+    valorCaja.value = '';
+
+}
 
 function generarNumeroSecreto() {
     let numeroSecreto = Math.floor(Math.random()*10) + 1;
@@ -27,8 +46,25 @@ function generarNumeroSecreto() {
 }
 
 
-asignarTextoElemento('h1', "Numero secreto");
-asignarTextoElemento('p', "Ingresa un numero del 1 al 10");
+function condicionesIniciales(){
+    asignarTextoElemento('h1', "Numero secreto");
+    asignarTextoElemento('p', "Ingresa un numero del 1 al 10");
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
 
+function reiniciarJuego(){
+    //limpiar la caja
+    limpiarCaja();
+    //indicar condiciones iniciales
+    condicionesIniciales(); 
+    //deshabilitar boton de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+    console.log(numeroSecreto);
+    
+}
+
+condicionesIniciales();
+console.log(numeroSecreto);
 
 
